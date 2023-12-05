46dd15f7d070b0ea9c126fd11c19b4541c36c9cf
Christina Stewart <christina.stewart@shopify.com>
2023-12-04 21:51:49 -0500

    make test pass by creating resident status class method

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,3 +1,9 @@
 class Resident < ApplicationRecord
   belongs_to :unit
+
+  def self.calculate_resident_status(move_in_date, move_out_date, for_date)
+    return 'future' if move_in_date > for_date
+    return 'current' if move_in_date <= for_date && (move_out_date.nil? || move_out_date > for_date)
+    'past'
+  end
 end
